5d53d1fcea396e463149c10b9d38b74ec609b787
mohamedsaid-sd <sudanit2015@gmail.com>
2023-11-12 19:22:00 +0200

    convert service to

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 	<title> الرئيسية </title>
 </head>
 <body>
-السلام علليكم محمد سيد
 <?php
 
 // go to ____ language default
 
 // Arabic
-//echo '<meta http-equiv="refresh" content="0;url=Arabic/index.html"';
+echo '<meta http-equiv="refresh" content="0;url=Arabic/index.html"';
 
 // English
 //echo '<meta http-equiv="refresh" content="0;url=English/index.html"';
