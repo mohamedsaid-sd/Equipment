64222342939906b5d50642e0c5233caf9effbc4d
mohamedsaid-sd <sudanit2015@gmail.com>
2023-11-12 16:36:43 +0200

    firest commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> الرئيسية </title>
+</head>
+<body>
+
+<?php
+
+// go to ____ language default
+
+// Arabic
+echo '<meta http-equiv="refresh" content="0;url=Arabic/index.html"';
+
+// English
+//echo '<meta http-equiv="refresh" content="0;url=English/index.html"';
+
+?>
+
+</body>
+</html>
